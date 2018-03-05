33e345c35fbc20bef625087eabbf7a2b6ba2f1e4
Aleksander Harhun <aleksanderharhun@gmail.com>
2018-03-05 20:06:12 +0100

    Add test cases for login and logout

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { LoginPage } from '../../components/LoginPage';
 import { shallow } from 'enzyme';
 
+let startLogin;
+
 test('shoud render LoginPage component correctly', () => {
     const result = shallow(<LoginPage />);
     expect(result).toMatchSnapshot();
 }); 
+
+test('shoud call startLogin on button click', () => {
+    startLogin = jest.fn();
+    const wrapper = shallow(
+        <LoginPage 
+            startLogin={startLogin}   
+        />
+    );
+    wrapper.find('button').simulate('click');
+    expect(startLogin).toHaveBeenCalled();
+});

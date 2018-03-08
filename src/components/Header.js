64d4af4edbe1665b429fd88f67143ff002a14ad6
Aleksander Harhun <aleksanderharhun@gmail.com>
2018-03-08 18:52:24 +0100

    Add PublicRoute

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,8 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
-        <p><NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink></p>
+        <p><NavLink to='/dashboard' activeClassName='is-active' >Dashboard</NavLink></p>
         <p><NavLink to='/create' activeClassName='is-active'>Create</NavLink></p>
-        <p><NavLink to='/edit' activeClassName='is-active'>edit page</NavLink></p>
-        <p><NavLink to='/help' activeClassName='is-active'>help</NavLink></p>
         <button onClick={startLogout}>Logout</button>
     </header>
 );
